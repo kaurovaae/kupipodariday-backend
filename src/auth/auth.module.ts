49152a6b1efdd969b7fc2626bfec86afb734493b
kaurovaae <kaurova.a.e@gmail.com>
2025-01-08 16:30:43 +0300

    fix: secret

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -15,7 +15,7 @@ import { LocalGuard } from '../guards/local.guard';
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>('jwt_secret'),
+        secret: configService.get<string>('JWT_SECRET'),
       }),
       inject: [ConfigService],
     }),
